3c2161164c2822ccc243bf1b6f30618f569ef02a
Manish Dhiman <dhimanmanish472@gmail.com>
2026-03-24 17:26:39 +0530

    added backend

--- client/src/lib/queryClient.ts
+++ client/src/lib/queryClient.ts
@@ -7,12 +7,14 @@ async function throwIfResNotOk(res: Response) {
   }
 }
 
+const BASE_URL = "https://indane-cylinder-management.onrender.com";
+
 export async function apiRequest(
   method: string,
   url: string,
   data?: unknown | undefined,
 ): Promise<Response> {
-  const res = await fetch(url, {
+  const res = await fetch(BASE_URL + url, {
     method,
     headers: data ? { "Content-Type": "application/json" } : {},
     body: data ? JSON.stringify(data) : undefined,
@@ -29,7 +31,7 @@ export const getQueryFn: <T>(options: {
 }) => QueryFunction<T> =
   ({ on401: unauthorizedBehavior }) =>
   async ({ queryKey }) => {
-    const res = await fetch(queryKey.join("/") as string, {
+    const res = await fetch(BASE_URL + (queryKey.join("/") as string), {
       credentials: "include",
     });
 
